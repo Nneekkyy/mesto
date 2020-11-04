6413ff691e964e140e170db92fc73064624c7017
dasd ed <neky@list.ru>
2020-11-04 15:09:33 +0500

    реализованы все функции по заданию

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,23 @@
 let nameOutput = document.querySelector('.profile__name');
 let titleOutput =  document.querySelector('.profile__title');
 let formElement = document.querySelector('.edit-form__fields');
+let cardElement = document.querySelector('.add-card__fields');
 let popupSelect = document.querySelector('.popup');
 let editForm = document.querySelector('.edit-form');
-let cardForm = document.querySelector('.edit-card');
+let cardForm = document.querySelector('.add-card');
 const cardContainer = document.querySelector('.elements__list');
+const cardTemplate = document.querySelector('#card').content;
+
 
 // переменные для всех кнопок
 let editButton = document.querySelector('.profile__edit');
 let closeFormButton = document.querySelector('.edit-form__close');
-let closeCardButton = document.querySelector('.edit-card__close');
+let closeCardButton = document.querySelector('.add-card__close');
 let saveButton = document.querySelector('.edit-form__button');
 let addCardButton = document.querySelector('.profile__add');
-let saveCardButton = document.querySelector('.edit-card__button');
+let saveCardButton = document.querySelector('.add-card__button');
 
+let currentDocument = document.currentScript.ownerDocument;
 //массив с карточками
 
 const initialCards = [
@@ -54,9 +58,16 @@ initialCards.forEach(function (element) {
   cardContainer.append(cardElement);
 });
 
-// function addCard () {
-//   initialCards.unshift('name: ${placeField.textContent} , link: ${sourceField.textContent}')
-// }
+function addCard () {
+  let newCard = initialCards.slice(0,1);
+  newCard.forEach(function (element) {
+    const cardTemplate = document.querySelector('#card').content;
+    const cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.element__image').src = element.link;
+    cardElement.querySelector('.element__name-title').textContent = element.name;
+    cardContainer.prepend(cardElement);
+  });
+ }
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -71,32 +82,66 @@ function formSubmitHandler (evt) {
     formClassToggle ();
 }
 
+function cardSubmitHandler (evt) {
+  evt.preventDefault();
+
+  initialCards.unshift({name: placeField.value , link: sourceField.value});
+  cardClassToggle ();
+  addCard();
+  placeField.value = '';
+  sourceField.value = '';
+  console.log (initialCards);
+}
+
+function popupShow() {
+  popupSelect.classList.toggle('popup_opened');
+}
+
 function cardClassToggle() {
-  if (popupSelect.classList.contains('popup_opened')) {
-      popupSelect.classList.toggle('popup_opened');
-      cardForm.classList.toggle('edit-card_opened');
-  } else {
-    popupSelect.classList.toggle('popup_opened');
-    cardForm.classList.toggle('edit-card_opened');
+  if (cardForm.classList.contains('add-card')) {
+      popupShow();
+      cardForm.classList.toggle('add-card_opened');
+
   }
 }
 
 function formClassToggle() {
-  if (popupSelect.classList.contains('popup_opened')) {
-      popupSelect.classList.toggle('popup_opened');
+  if (editForm.classList.contains('edit-form_opened')) {
+      popupShow();
       editForm.classList.toggle('edit-form_opened');
   } else {
-    popupSelect.classList.toggle('popup_opened');
+    popupShow();
     editForm.classList.toggle('edit-form_opened');
+
     nameField.value = nameOutput.textContent;
     titleField.value = titleOutput.textContent;
   }
 }
 
+
 // Прикрепляем обработчик к форме:
 formElement.addEventListener('submit', formSubmitHandler);
+cardElement.addEventListener('submit', cardSubmitHandler);
 
 addCardButton.addEventListener('click', cardClassToggle, false);
 editButton.addEventListener('click', formClassToggle, false);
 closeFormButton.addEventListener('click', formClassToggle, false);
 closeCardButton.addEventListener('click', cardClassToggle, false);
+
+
+
+currentDocument.addEventListener('click', function (event) {
+
+  if (event.target.classList.contains('element__button-like')) {
+    event.target.classList.toggle('element__button-like_active');
+  }
+});
+
+
+currentDocument.addEventListener('click', function (event) {
+
+  if (event.target.classList.contains('element__button-trash')) {
+    const currentDocument = event.target.closest('.element');
+    currentDocument.remove();
+  }
+});
