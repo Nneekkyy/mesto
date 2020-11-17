7864fbe8744dd34972e7ee854434336cd81cde32
Barrrsss <neky@list.ru>
2020-11-17 21:44:01 +0500

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 //импортируем карточки
 import {initialCards} from './CardsData.js';
-import {initialCards} from './validate.js';
+// import enableValidation from './validate.js';
 
 //определяем элементы
 //поля edit
@@ -169,3 +169,60 @@ document.addEventListener('keydown', function(event) {
 });
 
 document.addEventListener('click', (e) => console.log(e.target));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add('form__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove('form__input_type_error');
+  errorElement.classList.remove('form__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+function enableValidation () {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach((formElement) => {
+  formElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+  });
+
+    setEventListeners(formElement);
+});
+}
+enableValidation ();
