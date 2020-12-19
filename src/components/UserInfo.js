ee31c7d5a73c062329205f7ebb627bb2d026cbe7
Aleksey Krasnov <neky@list.ru>
2020-12-19 14:51:24 +0500

    feat: webpack

---
+++ src/components/UserInfo.js
@@ -0,0 +1,19 @@
+export default class UserInfo {
+    constructor({name, about}){
+        this._name = name;
+        this._about = about;
+    }
+
+    getUserInfo() {
+        this._profileInfo = {};
+        this._profileInfo.name = this._name.textContent;
+        this._profileInfo.about = this._about.textContent;
+
+       return this._profileInfo;
+    }
+
+    setUserInfo(profileData) {
+        this._name.textContent = profileData.name;
+        this._about.textContent = profileData.about;
+    }
+}
